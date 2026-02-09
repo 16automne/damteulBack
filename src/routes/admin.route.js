339a2b2c98d0a8f4adac20c1e070cdb80113e6cf
kweon <anybil30@gmail.com>
2026-02-09 11:07:48 +0900

    Reapply "채팅"
    This reverts commit 272150fd5d7a5ee29d603abc618356e5bb763393.

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin.controllers");
-const { getAdminEvents, getAdminNotices, getEventDetail, updateEvent, deleteEvent } = require("../controllers/admin.noticeEvent.controllers");
+const { getAdminEvents, getAdminNotices, getEventDetail, updateEvent, deleteEvent, getNoticeDetail, updateNotice, deleteNotice, createEvent, createNotice } = require("../controllers/admin.noticeEvent.controllers");
 
 
 
@@ -17,18 +17,18 @@ router.get("/dashboard", adminController.dashboard);
 // adminUsers
 router.get("/users", adminController.users);
 router.get("/users/:user_id", adminController.getUserDetail);
-router.delete("/users/:id", adminController.userDelete);
+router.put("/users/:id/delete", adminController.userDelete);
 
 // adminReports
 router.get("/reports", adminController.reports);
 router.get("/reports/:id", adminController.getReportsDetail);
 router.patch("/reports/:id", adminController.updateReportsDetail);
-router.delete("/reports/:id", adminController.reportDelete);
+router.put("/reports/:id/delete", adminController.reportDelete);
 
 // adminTrades
 router.get("/trades", adminController.trades);
 router.get("/trades/:id", adminController.getTradesDeatail);
-router.delete("/trades/:id", adminController.tradeDelete);
+router.put("/trades/:id/delete", adminController.tradeDelete);
 
 
 
@@ -38,23 +38,37 @@ router.get("/events", getAdminEvents);
 // 공지사항 목록
 router.get("/notices", getAdminNotices);
 
+// 공지사항 상세 가져오기
 router.get("/notice/:id", getNoticeDetail);
+
+// 공지사항 업데이트
 router.put("/notice/:id", updateNotice);  
+
+router.put("/notice/:id/delete", deleteNotice);
+
+router.post("/notice/create", createNotice);
 
 // 이벤트 상세 가져오기
 router.get("/event/:id", getEventDetail);
 
+// 이벤트 업데이트
 router.put("/event/:id", updateEvent);
-router.delete("/events/:id", deleteEvent)
+
+// 이벤트 삭제
+router.put("/events/:id/delete", deleteEvent);
+
+router.post("/event/create", createEvent);
+
+
 
 // admainCommu
 router.get("/community", adminController.community);
 router.get("/community/:id", adminController.getCommunityDetail);
-router.delete("/community/:id", adminController.communityDelete);
+router.put("/community/:id/delete", adminController.communityDelete);
 
 // adminPosts (젤 하단에 위치 시킬것)
 router.get("/posts", adminController.posts);
-router.delete("/:url/:id", adminController.postDelete);
+router.put("/:url/:id/delete", adminController.postDelete);
 router.get("/:cate/:id", adminController.getPostDetail);
 
 module.exports = router;
